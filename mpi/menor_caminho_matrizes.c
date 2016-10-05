d6647cca6835687a7dacf625070b29cfa895fff9
Leo Nascimento <leonardo_pinheiro12@hotmail.com>
2016-10-05 15:18:13 -0300

    Implementacao do menor caminho utilizando mpi

--- mpi/menor_caminho_matrizes.c
+++ mpi/menor_caminho_matrizes.c
@@ -1,18 +1,12 @@
-/* Menor caminho de Grafo pelo método da multiplicação de matrizes, utilizando MPI
-   Desenvolvido por: Leonardo Pinheiro do Nascimento
-
-   Entrada de dados: Matriz adjacente do grafo, gerado aleatóriamente pelo método inicializarMatrizAdjacente
-   Saída de dados: Matriz contendo as menores distâncias entre todos os vértices do grafo 
-
-*/
-
+#include "mpi.h"
 #include <stdio.h>
-#include <mpi.h>
 #include <stdlib.h>
- 
-#define TAM 10
+#define TAM 4
+#define MASTER 0               /* taskid of first task */
+#define FROM_MASTER 1          /* setting a message type */
+#define FROM_WORKER 2          /* setting a message type */
 
-void imprimirMatriz(int** m) {
+void imprimirMatriz(int m[TAM][TAM]) {
     int i, j;
     printf("    ");
     for (i = 0; i < TAM; ++i)
@@ -27,24 +21,8 @@ void imprimirMatriz(int** m) {
     printf("\n");
 }
 
-void zeraMatriz(int** m){
-    int i, j;
-    for (i = 0; i < TAM; i ++)
-        for (j = 0; j < TAM; j ++)
-            m[i][j] = 0;
-}
-
-//Alocação dinâmica de matrizes
-int** getNovaMatriz(){
-    int i;
-    int** m = (int**)malloc(TAM * sizeof(int*));
-    for (i = 0; i < TAM; i++) 
-           m[i] = (int*) malloc(TAM * sizeof(int)); 
-    return m; 
-}
-
 //Inicializa a matriz Adjacente com números randômicos entre 0 e 1
-void inicializaMatrizAdjacente(int** mAdjacente){
+void inicializaMatrizAdjacente(int  mAdjacente[TAM][TAM]){
     int i, j;
     srand( (unsigned)time(NULL));
     for (i = 0; i < TAM; i ++){
@@ -54,30 +32,15 @@ void inicializaMatrizAdjacente(int** mAdjacente){
     }
 }
 
-void copiaMatriz (int** mOrigem, int** mDestino){
+void copiaMatriz (int mOrigem[TAM][TAM], int mDestino[TAM][TAM]){
     int i,j;
     for(i = 0; i < TAM; i ++)
         for(j = 0; j < TAM; j ++)
             mDestino[i][j] = mOrigem[i][j]; 
 }
-//Realiza a multiplicação de duas matrizes
-void multiplicarMatrizes(int** m1, int** m2, int** mResult){
-    int i, j, k;
-    MPI_Bcast (m2, TAM*TAM, MPI_INT, 0, MPI_COMM_WORLD);
-    MPI_Scatter (m1, TAM*TAM, MPI_INT, m1[0], TAM*TAM, MPI_INT, 0, MPI_COMM_WORLD);
-    for (i = 0; i < TAM; i ++)
-       for(j = 0; j < TAM; j ++)
-            for(k = 0; k < TAM; k ++)
-                mResult[i][j] += m1[i][k] * m2[k][j];
-    MPI_Gather (mResult[0], TAM*TAM, MPI_INT, mResult, TAM*TAM, MPI_INT, 0, MPI_COMM_WORLD);
-       
-}
 
-//Atualiza a matriz de distâncias, de acordo com o grau fornecido
-//O Grau será o número da iteração do for dentro de calculaDistancias
-void atualizaMatrizDistancias(int** mDistancias, int** mNovasDistancias, int grau){
+void atualizaMatrizDistancias(int mDistancias[TAM][TAM], int mNovasDistancias[TAM][TAM], int grau){
     int i, j;
-    //#pragma omp parallel private(i, j)
     for (i = 0; i < TAM; i ++)
        for(j = 0; j < TAM; j ++)
             //Registra um novo caminho, caso haja
@@ -86,41 +49,99 @@ void atualizaMatrizDistancias(int** mDistancias, int** mNovasDistancias, int gra
     
 }
 
-void calculaDistancias(int** mAdjacente, int** mDistancias){
-    int h;
-    int** mAuxiliar = getNovaMatriz(); //Matriz que vai 
-    int** mResult = getNovaMatriz();
-    zeraMatriz(mResult);
-    copiaMatriz(mAdjacente, mAuxiliar);
-    copiaMatriz(mAdjacente, mDistancias); //Primeira matriz de distâncias é a matriz adjacente
-    //Multiplica a matriz TAM - 1 vezes
-    for(h = 2; h <= TAM; h ++){
-        multiplicarMatrizes(mAdjacente, mAuxiliar, mResult);
-        //Faz a atualização das novas distâncias
-        //A matriz contendo as novas distâncias eh a matriz resultante da multiplicação
-        atualizaMatrizDistancias(mDistancias, mResult, h); 
-        copiaMatriz(mResult, mAuxiliar);
-        zeraMatriz(mResult);
-    }
+int main (int argc, char *argv[]){
+int totalTask,              /* number of tasks in partition */
+  taskid,                /* a task identifier */
+  numworkers,            /* number of worker tasks */
+  source,                /* task id of message source */
+  dest,                  /* task id of message destination */
+  mtype,                 /* message type */
+  rows,                  /* rows of matrix A sent to each worker */
+  averow, extra, offset, /* used to determine rows sent to each worker */
+  h,i, j, k, rc;           /* misc */
+int a[TAM][TAM], b[TAM][TAM],c[TAM][TAM],d[TAM][TAM];
 
+MPI_Status status;
+
+MPI_Init(&argc,&argv);
+MPI_Comm_rank(MPI_COMM_WORLD,&taskid);
+MPI_Comm_size(MPI_COMM_WORLD,&totalTask);
+
+//Inicializacao das matrizes
+if(taskid  == MASTER){
+    numworkers = totalTask-1; //Ignoro a master
+    inicializaMatrizAdjacente(a);
+    printf("Matriz Adjacente Gerada aleatoriamente...\n");
+    imprimirMatriz(a);
+    copiaMatriz(a, b);
+    copiaMatriz(a, d);//Primeira matriz de distâncias é a matriz adjacente
 }
+//Multiplica a matriz TAM - 1 vezes
+for(h = 2; h <= TAM; h ++){
+ /**************************** master task ************************************/
+   if (taskid == MASTER)
+   {
+      printf("Iteracao %d ...\n", h);  
+      //Aqui irei dividir a matriz e distribuir pelas threads
+      averow = TAM/numworkers;
+      extra = TAM%numworkers;
+      offset = 0;
+      mtype = FROM_MASTER;
+      for (dest=1; dest<=numworkers; dest++)
+      {
+         rows = (dest <= extra) ? averow+1 : averow;    
+         printf("Sending %d rows to task %d offset=%d\n",rows,dest,offset);
+         MPI_Send(&offset, 1, MPI_INT, dest, mtype, MPI_COMM_WORLD);
+         MPI_Send(&rows, 1, MPI_INT, dest, mtype, MPI_COMM_WORLD);
+         MPI_Send(&a[offset][0], rows*TAM, MPI_INT, dest, mtype,
+                   MPI_COMM_WORLD);
+         MPI_Send(&b, TAM*TAM, MPI_INT, dest, mtype, MPI_COMM_WORLD);
+         offset = offset + rows;
+      }
 
-int main(int argc, char * argv[]){
-    double start,stop;
-    int** matrizAdjacente  = getNovaMatriz();
-    int** matrizDistancias = getNovaMatriz();
-    int p, myrank;
-    MPI_Init (&argc, &argv);
-    MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
-    MPI_Comm_size(MPI_COMM_WORLD, &p);
-    printf("%d\n",p);
-    printf("%d\n",myrank);
-    inicializaMatrizAdjacente(matrizAdjacente);
-    zeraMatriz(matrizDistancias);
-    imprimirMatriz(matrizAdjacente);   
-    start = clock();
-    calculaDistancias(matrizAdjacente, matrizDistancias);
-    imprimirMatriz(matrizDistancias);
-    stop = clock();
-    printf("tempo %f\n",stop-start);
+      /* Receive results from worker tasks */
+      mtype = FROM_WORKER;
+      for (i=1; i<=numworkers; i++)
+      {
+         source = i;
+         MPI_Recv(&offset, 1, MPI_INT, source, mtype, MPI_COMM_WORLD, &status);
+         MPI_Recv(&rows, 1, MPI_INT, source, mtype, MPI_COMM_WORLD, &status);
+         MPI_Recv(&c[offset][0], rows*TAM, MPI_INT, source, mtype, 
+                  MPI_COMM_WORLD, &status);
+         printf("Received results from task %d\n",source);
+      }
+      atualizaMatrizDistancias(d, c, h);
+      copiaMatriz(c, b);
+      /* Print results */
+      printf("******************************************************\n");
+      printf("Matrix de distâncias atual:\n");
+      imprimirMatriz(d);
+      printf("\n******************************************************\n");
+   }
+
+/**************************** worker task ************************************/
+   if (taskid > MASTER)
+   {
+      mtype = FROM_MASTER;
+      MPI_Recv(&offset, 1, MPI_INT, MASTER, mtype, MPI_COMM_WORLD, &status);
+      MPI_Recv(&rows, 1, MPI_INT, MASTER, mtype, MPI_COMM_WORLD, &status);
+      MPI_Recv(&a, rows*TAM, MPI_INT, MASTER, mtype, MPI_COMM_WORLD, &status);
+      MPI_Recv(&b, TAM*TAM, MPI_INT, MASTER, mtype, MPI_COMM_WORLD, &status);
+
+      for (k=0; k<TAM; k++)
+         for (i=0; i<rows; i++)
+         {
+            c[i][k] = 0.0;
+            for (j=0; j<TAM; j++)
+               c[i][k] = c[i][k] + a[i][j] * b[j][k];
+         }
+      mtype = FROM_WORKER;
+      MPI_Send(&offset, 1, MPI_INT, MASTER, mtype, MPI_COMM_WORLD);
+      MPI_Send(&rows, 1, MPI_INT, MASTER, mtype, MPI_COMM_WORLD);
+      MPI_Send(&c, rows*TAM, MPI_INT, MASTER, mtype, MPI_COMM_WORLD);
+   }
+  
+}
+MPI_Finalize();
+
 }
--- mpi/multiplica_matrizes.c
+++ mpi/multiplica_matrizes.c
@@ -14,10 +14,7 @@
 #include "mpi.h"
 #include <stdio.h>
 #include <stdlib.h>
-
-#define NRA 62                 /* number of rows in matrix A */
-#define NCA 15                 /* number of columns in matrix A */
-#define NCB 7                  /* number of columns in matrix B */
+#define TAM 10
 #define MASTER 0               /* taskid of first task */
 #define FROM_MASTER 1          /* setting a message type */
 #define FROM_WORKER 2          /* setting a message type */
@@ -32,41 +29,40 @@ int numtasks,              /* number of tasks in partition */
   mtype,                 /* message type */
   rows,                  /* rows of matrix A sent to each worker */
   averow, extra, offset, /* used to determine rows sent to each worker */
-  i, j, k, rc;           /* misc */
-double  a[NRA][NCA],           /* matrix A to be multiplied */
-  b[NCA][NCB],           /* matrix B to be multiplied */
-  c[NRA][NCB];           /* result matrix C */
+  h,i, j, k, rc;           /* misc */
+double  a[TAM][TAM],           /* matrix A to be multiplied */
+  b[TAM][TAM],           /* matrix B to be multiplied */
+  c[TAM][TAM];           /* result matrix C */
 MPI_Status status;
 
 MPI_Init(&argc,&argv);
 MPI_Comm_rank(MPI_COMM_WORLD,&taskid);
 MPI_Comm_size(MPI_COMM_WORLD,&numtasks);
-
 if (numtasks < 2 ) {
   printf("Need at least two MPI tasks. Quitting...\n");
   MPI_Abort(MPI_COMM_WORLD, rc);
   exit(1);
   }
+ numworkers = numtasks-1;
 
+for(h = 1; h <= TAM; h ++){
+ 
 
-numworkers = numtasks-1;
-
-
-/**************************** master task ************************************/
+   /**************************** master task ************************************/
    if (taskid == MASTER)
    {
       printf("mpi_mm has started with %d tasks.\n",numtasks);
       printf("Initializing arrays...\n");
-      for (i=0; i<NRA; i++)
-         for (j=0; j<NCA; j++)
+      for (i=0; i<TAM; i++)
+         for (j=0; j<TAM; j++)
             a[i][j]= i+j;
-      for (i=0; i<NCA; i++)
-         for (j=0; j<NCB; j++)
+      for (i=0; i<TAM; i++)
+         for (j=0; j<TAM; j++)
             b[i][j]= i*j;
 
       /* Send matrix data to the worker tasks */
-      averow = NRA/numworkers;
-      extra = NRA%numworkers;
+      averow = TAM/numworkers;
+      extra = TAM%numworkers;
       offset = 0;
       mtype = FROM_MASTER;
       for (dest=1; dest<=numworkers; dest++)
@@ -75,9 +71,9 @@ numworkers = numtasks-1;
          printf("Sending %d rows to task %d offset=%d\n",rows,dest,offset);
          MPI_Send(&offset, 1, MPI_INT, dest, mtype, MPI_COMM_WORLD);
          MPI_Send(&rows, 1, MPI_INT, dest, mtype, MPI_COMM_WORLD);
-         MPI_Send(&a[offset][0], rows*NCA, MPI_DOUBLE, dest, mtype,
+         MPI_Send(&a[offset][0], rows*TAM, MPI_DOUBLE, dest, mtype,
                    MPI_COMM_WORLD);
-         MPI_Send(&b, NCA*NCB, MPI_DOUBLE, dest, mtype, MPI_COMM_WORLD);
+         MPI_Send(&b, TAM*TAM, MPI_DOUBLE, dest, mtype, MPI_COMM_WORLD);
          offset = offset + rows;
       }
 
@@ -88,7 +84,7 @@ numworkers = numtasks-1;
          source = i;
          MPI_Recv(&offset, 1, MPI_INT, source, mtype, MPI_COMM_WORLD, &status);
          MPI_Recv(&rows, 1, MPI_INT, source, mtype, MPI_COMM_WORLD, &status);
-         MPI_Recv(&c[offset][0], rows*NCB, MPI_DOUBLE, source, mtype, 
+         MPI_Recv(&c[offset][0], rows*TAM, MPI_DOUBLE, source, mtype, 
                   MPI_COMM_WORLD, &status);
          printf("Received results from task %d\n",source);
       }
@@ -96,10 +92,10 @@ numworkers = numtasks-1;
       /* Print results */
       printf("******************************************************\n");
       printf("Result Matrix:\n");
-      for (i=0; i<NRA; i++)
+      for (i=0; i<TAM; i++)
       {
          printf("\n"); 
-         for (j=0; j<NCB; j++) 
+         for (j=0; j<TAM; j++) 
             printf("%6.2f   ", c[i][j]);
       }
       printf("\n******************************************************\n");
@@ -113,20 +109,23 @@ numworkers = numtasks-1;
       mtype = FROM_MASTER;
       MPI_Recv(&offset, 1, MPI_INT, MASTER, mtype, MPI_COMM_WORLD, &status);
       MPI_Recv(&rows, 1, MPI_INT, MASTER, mtype, MPI_COMM_WORLD, &status);
-      MPI_Recv(&a, rows*NCA, MPI_DOUBLE, MASTER, mtype, MPI_COMM_WORLD, &status);
-      MPI_Recv(&b, NCA*NCB, MPI_DOUBLE, MASTER, mtype, MPI_COMM_WORLD, &status);
+      MPI_Recv(&a, rows*TAM, MPI_DOUBLE, MASTER, mtype, MPI_COMM_WORLD, &status);
+      MPI_Recv(&b, TAM*TAM, MPI_DOUBLE, MASTER, mtype, MPI_COMM_WORLD, &status);
 
-      for (k=0; k<NCB; k++)
+      for (k=0; k<TAM; k++)
          for (i=0; i<rows; i++)
          {
             c[i][k] = 0.0;
-            for (j=0; j<NCA; j++)
+            for (j=0; j<TAM; j++)
                c[i][k] = c[i][k] + a[i][j] * b[j][k];
          }
       mtype = FROM_WORKER;
       MPI_Send(&offset, 1, MPI_INT, MASTER, mtype, MPI_COMM_WORLD);
       MPI_Send(&rows, 1, MPI_INT, MASTER, mtype, MPI_COMM_WORLD);
-      MPI_Send(&c, rows*NCB, MPI_DOUBLE, MASTER, mtype, MPI_COMM_WORLD);
+      MPI_Send(&c, rows*TAM, MPI_DOUBLE, MASTER, mtype, MPI_COMM_WORLD);
    }
-   MPI_Finalize();
+  
+}
+MPI_Finalize();
+
 }
