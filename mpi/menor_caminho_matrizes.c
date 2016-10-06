543659f8c366228b80d798822e7d76f49632856d
Leo Nascimento <leonardo_pinheiro12@hotmail.com>
2016-10-06 15:55:18 -0300

    Finalizada implementacao mpi

--- mpi/menor_caminho_matrizes.c
+++ mpi/menor_caminho_matrizes.c
@@ -61,20 +61,8 @@ void multiplicarMatrizes(int m1[TAM][TAM], int m2[TAM][TAM], int mResult[TAM][TA
 }
 
 int main (int argc, char *argv[]){
-    int totalTask,           /* number of tasks in partition */
-      taskid,                /* a task identifier */
-      numworkers,            /* number of worker tasks */
-      source,                /* task id of message source */
-      dest,                  /* task id of message destination */
-      mtype,                 /* message type */
-      rows,                  /* rows of matrix A sent to each worker */
-      averow, extra, offset, /* used to determine rows sent to each worker */
-      h,i, j, k, rc;           /* misc */
-    int mAdjacente[TAM][TAM];
-    int mAuxiliar[TAM][TAM];
-    int mResult[TAM][TAM];
-    int mDistancias[TAM][TAM];
-
+    int totalTask, taskid, origem, dest, totalThreads, rows, lastRow, grau;         
+    int mAdjacente[TAM][TAM], mAuxiliar[TAM][TAM], mResult[TAM][TAM], mDistancias[TAM][TAM];
     MPI_Status status;
 
     MPI_Init(&argc,&argv);
@@ -82,8 +70,8 @@ int main (int argc, char *argv[]){
     MPI_Comm_size(MPI_COMM_WORLD,&totalTask);
 
     //Inicializacao das matrizes
-    if(taskid  == 0){
-        numworkers = totalTask-1; //Ignoro a master
+    if(taskid  == MASTER){
+        totalThreads = totalTask - 1; //Ignoro a master
         printf("Matriz Adjacente Gerada aleatoriamente...\n");
         inicializaMatrizAdjacente(mAdjacente);
         imprimirMatriz(mAdjacente);
@@ -91,56 +79,47 @@ int main (int argc, char *argv[]){
         copiaMatriz(mAdjacente, mDistancias);//Primeira matriz de distâncias é a matriz adjacente 
     }
     //Multiplica a matriz TAM - 1 vezes
-    for(h = 2; h <= TAM; h ++){
-       if (taskid == MASTER)
-       {
-          printf("Iteracao %d ...\n", h);  
-          averow = TAM/numworkers;
-          extra = TAM%numworkers;
-          offset = 0;
+    for(grau = 2; grau <= TAM; grau++){
+       if (taskid == MASTER){
+          printf("-----------------------------------------------------\n");
+          printf("Iteracao %d ...\n", grau);  
+          lastRow = 0;
           
           //Aqui irei dividir a matriz e distribuir pelas threads
-          for (dest=1; dest<=numworkers; dest++)
-          {
-             rows = (dest <= extra) ? averow+1 : averow;    
-             printf("Enviando %d linhas para a thread %d \n",rows,dest);
-             MPI_Send(&offset, 1, MPI_INT, dest, FROM_MASTER, MPI_COMM_WORLD);
+          for (dest=1; dest<=totalThreads; dest++){
+             if(dest <= TAM%totalThreads)
+                rows = (TAM/totalThreads) + 1;
+             else
+                rows = (TAM/totalThreads);
+             MPI_Send(&lastRow, 1, MPI_INT, dest, FROM_MASTER, MPI_COMM_WORLD);
              MPI_Send(&rows, 1, MPI_INT, dest, FROM_MASTER, MPI_COMM_WORLD);
-             MPI_Send(&mAdjacente[offset][0], rows*TAM, MPI_INT, dest, FROM_MASTER, MPI_COMM_WORLD);
+             MPI_Send(&mAdjacente[lastRow][0], rows*TAM, MPI_INT, dest, FROM_MASTER, MPI_COMM_WORLD);
              MPI_Send(&mAuxiliar, TAM*TAM, MPI_INT, dest, FROM_MASTER, MPI_COMM_WORLD);
-             offset = offset + rows;
+             lastRow += rows;
           }
           //Recebo a matriz resultante da multiplicação
-          for (i=1; i<=numworkers; i++)
-          {
-             source = i;
-             MPI_Recv(&offset, 1, MPI_INT, source, FROM_WORKER, MPI_COMM_WORLD, &status);
-             MPI_Recv(&rows, 1, MPI_INT, source, FROM_WORKER, MPI_COMM_WORLD, &status);
-             MPI_Recv(&mResult[offset][0], rows*TAM, MPI_INT, source, FROM_WORKER, MPI_COMM_WORLD, &status); 
-             printf("Resultados recebidos da thread %d\n",source);
+          for (origem=1; origem<= totalThreads; origem++){
+             MPI_Recv(&lastRow, 1, MPI_INT, origem, FROM_WORKER, MPI_COMM_WORLD, &status);
+             MPI_Recv(&rows, 1, MPI_INT, origem, FROM_WORKER, MPI_COMM_WORLD, &status);
+             MPI_Recv(&mResult[lastRow][0], rows*TAM, MPI_INT, origem, FROM_WORKER, MPI_COMM_WORLD, &status); 
           }
-          atualizaMatrizDistancias(mDistancias, mResult, h);
+          atualizaMatrizDistancias(mDistancias, mResult, grau);
           copiaMatriz(mResult, mAuxiliar);
-          printf("******************************************************\n");
           printf("Matriz de distâncias atual:\n");
           imprimirMatriz(mDistancias);
-          printf("\n******************************************************\n");
        }
 
-       if (taskid > MASTER)
-       {
-          mtype = FROM_MASTER;
-          MPI_Recv(&offset, 1, MPI_INT, MASTER, mtype, MPI_COMM_WORLD, &status);
-          MPI_Recv(&rows, 1, MPI_INT, MASTER, mtype, MPI_COMM_WORLD, &status);
-          MPI_Recv(&mAdjacente, rows*TAM, MPI_INT, MASTER, mtype, MPI_COMM_WORLD, &status);
-          MPI_Recv(&mAuxiliar, TAM*TAM, MPI_INT, MASTER, mtype, MPI_COMM_WORLD, &status);
+       if (taskid > MASTER){
+          MPI_Recv(&lastRow, 1, MPI_INT, MASTER, FROM_MASTER, MPI_COMM_WORLD, &status);
+          MPI_Recv(&rows, 1, MPI_INT, MASTER, FROM_MASTER, MPI_COMM_WORLD, &status);
+          MPI_Recv(&mAdjacente, rows*TAM, MPI_INT, MASTER, FROM_MASTER, MPI_COMM_WORLD, &status);
+          MPI_Recv(&mAuxiliar, TAM*TAM, MPI_INT, MASTER, FROM_MASTER, MPI_COMM_WORLD, &status);
           
           multiplicarMatrizes(mAdjacente, mAuxiliar, mResult, rows);
           
-          mtype = FROM_WORKER;
-          MPI_Send(&offset, 1, MPI_INT, MASTER, mtype, MPI_COMM_WORLD);
-          MPI_Send(&rows, 1, MPI_INT, MASTER, mtype, MPI_COMM_WORLD);
-          MPI_Send(&mResult, rows*TAM, MPI_INT, MASTER, mtype, MPI_COMM_WORLD);
+          MPI_Send(&lastRow, 1, MPI_INT, MASTER, FROM_WORKER, MPI_COMM_WORLD);
+          MPI_Send(&rows, 1, MPI_INT, MASTER, FROM_WORKER, MPI_COMM_WORLD);
+          MPI_Send(&mResult, rows*TAM, MPI_INT, MASTER, FROM_WORKER, MPI_COMM_WORLD);
        }
       
     }
